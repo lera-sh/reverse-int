8a0325373fe1a265461d4f48b6a5f66dd96856f9
Valeria Shirko <valerka.shirko@gmail.com>
2020-10-24 01:47:00 +0300

    feat: add solutions to test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  return (n > 0) ? 
+    parseFloat(
+      n.toString().split('').reverse().join('')
+    ) * Math.sign(n) : 
+    parseFloat(
+      n.toString().split('').reverse().join('')
+    ) * Math.sign(-n)
 }
